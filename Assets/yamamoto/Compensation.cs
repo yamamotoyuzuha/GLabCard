9dd947a75374ecf6989ac83afb8e2bb5411725a6
yuzu1012 <yuzusonyuzuha@gmail.com>
2025-06-02 20:25:36 +0900

    代償カードのスクリプトを少し変更

--- Assets/yamamoto/Compensation.cs
+++ Assets/yamamoto/Compensation.cs
@@ -1,4 +1,5 @@
-﻿using Unity.VisualScripting;
+﻿using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,7 +16,7 @@ public class Compensation : UniqueEffect
         int damage = (int)(hit * (defense * card.Base.ComMagnification));
 
         player.Life -= player.Compensation; //代償でHPを引く
-
+        
         enemy.Base.EnemyLife -= damage;
         message.text = $"{damage}代償ダメージを与えた";
 
@@ -28,8 +29,13 @@ public class Compensation : UniqueEffect
     //一枚前のカードの追加効果
     public float FlontBuff(Card card, Card flontCard)
     {
-        float compensationValue = (int)card.Base.CardStatus.Attack_Status;
+        //Swordのattack値を参照する
+        var obj = GameObject.Find("CardGenerator");
+        var cardGe = obj.GetComponent<CardGenerator>();
+        var getSword = cardGe.CardBases.Find(x => x.CardName == "Sword");
 
+        float compensationValue = (int)getSword.CardStatus.Attack_Status;
+        
         if(flontCard == null)
         {
             return (int) compensationValue;
